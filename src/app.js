4cf71af23e3a80737976ee4da3bf42468c6a2958
Jazmin <jbedoya628@gmail.com>
2025-08-07 11:54:44 +0200

    trabajp terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+        const palos = ["♦", "♥", "♠", "♣"];
+        const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "Q", "k", "A"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+        const generarNumeroRandom = () => {
+          return Math.floor(Math.random() * numeros.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+        };
+        const generarPaloRandom = () => {
+          return Math.floor(Math.random() * palos.length);
+
+        };
+
+
+
+        //Elwgir numeros y palos
+        const paloElegido = palos[generarPaloRandom()];
+        const numeroElegido = numeros[generarNumeroRandom()];
+
+
+    
+
+      //Obtener Elementos
+      const paloArriba = document.querySelector("#paloArriba");
+      const numero = document.querySelector("#numero");
+      const paloAbajo = document.querySelector("#paloAbajo");
+      const carta = document.querySelector(".card");
+
+
+
+//Asignar Contenido
+paloArriba.textContent = paloElegido;
+numero.textContent = numeroElegido;
+paloAbajo.textContent = paloElegido;
+
+
+//estilos
+paloArriba.style.textAlign = "left";
+paloArriba.style.height = "50px";
+
+numero.style.textAlign = "center";
+numero.style.height = "80px";
+
+
+paloAbajo.style.textAlign = "right";
+paloAbajo.style.height = "50px";
+
+//Color segun el palo
+if (paloElegido === "♥" || paloAbajo === "♦") {
+  carta.style.color = "red";
+} else {
+  carta.style.color = "black";
+}
+
 };
